d8d3a366c326404ca59fd30ce9b6bae63798f3cd
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-25 17:15:11 +0100

    arm64: Enable frequency invariant scheduler load-tracking support
    Defines arch_scale_freq_capacity() to use cpufreq implementation.
    
    Including <linux/cpufreq.h> in topology.h like for the arm arch doesn't
    work because of CONFIG_COMPAT=y (Kernel support for 32-bit EL0).
    That's why cpufreq_scale_freq_capacity() has to be declared extern in
    topology.h.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I492c1851063220336b4ef6834303f2d44a6daf7a
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -24,9 +24,12 @@ void init_cpu_topology(void);
 void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
 
+#ifdef CONFIG_CPU_FREQ
+#define arch_scale_freq_capacity cpufreq_scale_freq_capacity
 struct sched_domain;
-extern unsigned long arch_scale_freq_capacity(struct sched_domain *sd, int cpu);
+extern unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 DECLARE_PER_CPU(atomic_long_t, cpu_freq_capacity);
+#endif
 
 #else
 
