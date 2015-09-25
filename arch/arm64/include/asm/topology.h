e839df84b9e2a7683a7b2e690320fc05bd83003a
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-25 17:34:15 +0100

    arm64: Enable max freq invariant scheduler load-tracking and capacity support
    Maximum Frequency Invariance has to be part of Cpu Invariance because
    Frequency Invariance deals only with differences in load-tracking
    introduces by Dynamic Frequency Scaling and not with limiting the
    possible range of cpu frequency.
    
    By placing Maximum Frequency Invariance into Cpu Invariance,
    load-tracking is scaled via arch_scale_cpu_capacity()
    in __update_load_avg() and cpu capacity is scaled via
    arch_scale_cpu_capacity() in update_cpu_capacity().
    
    To be able to save the extra multiplication in the scheduler hotpath
    (__update_load_avg()) we could:
    
      1 Inform cpufreq about base cpu capacity at boot and let it handle
        scale_cpu_capacity() as well.
      2 Use the cpufreq policy callback which would update a per-cpu current
        cpu_scale and this value would be return in scale_cpu_capacity().
      3 Use per-cpu current max_freq_scale and current cpu_scale with the
        current patch.
    
    Including <linux/cpufreq.h> in topology.h like for the arm arch doesn't
    work because of CONFIG_COMPAT=y (Kernel support for 32-bit EL0).
    That's why cpufreq_scale_max_freq_capacity() has to be declared extern
    in topology.h.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I2c25fbc7748865651975ba212e1d9b2976d8f7d6
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -28,6 +28,7 @@ const struct cpumask *cpu_coregroup_mask(int cpu);
 #define arch_scale_freq_capacity cpufreq_scale_freq_capacity
 struct sched_domain;
 extern unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
+extern unsigned long cpufreq_scale_max_freq_capacity(int cpu);
 DECLARE_PER_CPU(atomic_long_t, cpu_freq_capacity);
 #endif
 
