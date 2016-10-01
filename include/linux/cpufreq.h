9ff2f89735aaa6d1b306b6b9516cacc1e884acaa
Jake Whatley <jacob.whatley91@gmail.com>
2016-10-01 22:13:23 +0200

    include/linux/cpufreq.h: changes from NBQ_00WW_1_440

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -490,4 +490,12 @@ static inline int cpufreq_generic_exit(struct cpufreq_policy *policy)
 
 void acct_update_power(struct task_struct *p, cputime_t cputime);
 
+//20150330 FAO-537 IsonYHHung start
+struct _cpufreq_subsys_opt {
+	unsigned int (*cpuload)(struct cpufreq_policy *p, unsigned int load);
+	unsigned int (*freq_tuning)(struct cpufreq_policy *p, unsigned int f);
+};
+void register_cpufreq_subsys_opt(struct _cpufreq_subsys_opt *opt);
+//20150330 FAO-537 IsonYHHung end
+
 #endif /* _LINUX_CPUFREQ_H */
