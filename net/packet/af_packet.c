9d0750fb3be923e250ae938a18b7a636ae74e55c
Christopher R. Palmer <crpalmer@gmail.com>
2017-05-03 09:55:32 -0400

    packet: Fix attempt to fix races in fanout_release
    The prior code attempts to dereference f when it is NULL.
    
    Change-Id: I6952b69d94f048e03904599d74eb1b564c44d9f7

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1371,13 +1371,14 @@ static void fanout_release(struct sock *sk)
 
 	mutex_lock(&fanout_mutex);
 	f = po->fanout;
-	if (f)
+	if (f) {
 		po->fanout = NULL;
 
-	if (atomic_dec_and_test(&f->sk_ref)) {
-		list_del(&f->list);
-		dev_remove_pack(&f->prot_hook);
-		kfree(f);
+		if (atomic_dec_and_test(&f->sk_ref)) {
+			list_del(&f->list);
+			dev_remove_pack(&f->prot_hook);
+			kfree(f);
+		}
 	}
 	mutex_unlock(&fanout_mutex);
 }
